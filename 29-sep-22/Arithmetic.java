d8558db2c137f9de77deec51a45411aaa63f3eec
suchiworks <111557185+suchiworks@users.noreply.github.com>
2022-09-29 13:28:57 +0530

    Add files via upload

---
+++ 29-sep-22/Arithmetic.java
@@ -0,0 +1,21 @@
+package labRec29;
+
+public class Arithmetic {
+
+		 int add(int a, int b) {
+			    return a + b;
+			  }
+			}
+
+			class Adder extends Arithmetic {
+			  
+				public class sum {
+					 public void main(String[] args) {
+					
+				Adder d=new Adder();
+				System.out.println("Result="+ d.add(5, 10));
+					}
+
+				}
+
+}
