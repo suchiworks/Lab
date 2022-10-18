81fcc05f0232e0aa861ba64d2300f3eabfa5424a
suchiworks <111557185+suchiworks@users.noreply.github.com>
2022-10-18 10:29:24 +0530

    Add files via upload

---
+++ 17-oct-22/Foreachloopeg.java
@@ -0,0 +1,17 @@
+package lab17oct;
+
+public class Foreachloopeg {
+
+	public static void main(String[] args) {
+		// declaring array
+int sum=0;
+int arr[]= {1,2,3,4,5};// 1+2+3+4+5
+for(int i:arr) {
+	System.out.println(i);
+	sum=sum+i;}
+System.out.println("sum is:" +sum);
+
+}
+	}
+
+
